6113f8f30cfbe0e8a96a969bb539ae8b2d46a46b
Main Ostrich <sunsevev@gmail.com>
2024-05-24 18:27:00 +0500

    ARP Handling

--- netSimulator/netSimulator/NSimulator.cpp
+++ netSimulator/netSimulator/NSimulator.cpp
@@ -46,33 +46,85 @@ void NSimulator::GetPacketsLoop()
         {
             ProccesARP(pkt_data, header->len);
         }
+        else if (IsICMP(pkt_data, header->len))
+        {
+            ProccessICMP(pkt_data, header->len);
+        }
     }
 }
 
-bool NSimulator::IsARP(const u_char* pkt_data, int pkt_size)
+inline bool NSimulator::IsARP(const u_char* pkt_data)
 {
     uint16_t* type = (uint16_t*)(pkt_data + PACKET_TYPE_POS);
 
-    if (*type == ARP_PACKET_TYPE)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return (ArpTypes)*type == ArpTypes::ARP;
 }
 
-void NSimulator::ProccesARP(const u_char* pkt_data, int pkt_size)
+inline bool NSimulator::IsICMP(const u_char* pkt_data, int pktSize)
 {
-    uint16_t* operation = (uint16_t*)(pkt_data + ARP_OPERATION_POS);
+    return false;
+}
 
-    switch (*operation)
+void NSimulator::ProccessICMP(const u_char* pkt_data, int pktSize)
+{
+}
+
+void NSimulator::ProccessArpRequest(const u_char* pkt_data)
+{
+    uint32_t* senderIP = (uint32_t*)(pkt_data + (int)ArpPositions::SENDER_IP);
+    MAC* senderMAC = (MAC*)(pkt_data + (int)ArpPositions::SENDER_MAC);
+
+    arpTable[*senderIP] = *senderMAC;
+
+    uint8_t reply[42];
+    memcpy(reply, senderMAC, sizeof(MAC));
+    memcpy(reply + sizeof(MAC), netFor[*senderIP].mac.bytes, sizeof(MAC));
+
+    reply[12] = 0x08;
+    reply[13] = 0x06;
+
+    reply[14] = 0x00;
+    reply[15] = 0x01;
+
+    reply[16] = 0x08;
+    reply[17] = 0x00;
+
+    reply[18] = 0x06;
+
+    reply[19] = 0x04;
+
+    reply[20] = 0x00;
+    reply[21] = 0x02;
+
+    memcpy(reply + (int)ArpPositions::SENDER_MAC, netFor[*senderIP].mac.bytes, sizeof(MAC));
+    memcpy(reply + (int)ArpPositions::SENDER_IP, &netFor[*senderIP].ip, sizeof(uint32_t));
+
+    memcpy(reply + (int)ArpPositions::TARGET_MAC, senderMAC->bytes, sizeof(MAC));
+    memcpy(reply + (int)ArpPositions::TARGET_IP, senderIP, sizeof(uint32_t));
+
+    pcap_sendpacket(adhandle, reply, 42);
+}
+
+void NSimulator::ProccessArpReply(const u_char* pkt_data)
+{
+    uint32_t* senderIP = (uint32_t*)(pkt_data + (int)ArpPositions::SENDER_IP);
+    MAC* senderMAC = (MAC*)(pkt_data + (int)ArpPositions::SENDER_MAC);
+
+    arpTable[*senderIP] = *senderMAC;
+}
+
+void NSimulator::ProccesARP(const u_char* pkt_data)
+{
+    uint16_t* operation = (uint16_t*)(pkt_data + (int)ArpPositions::OPERATION);
+
+    switch ((ArpTypes)(*operation))
     {
-    case ARP_REQUEST:
+    case ArpTypes::REQUEST:
+        ProccessArpRequest(pkt_data);
         break;
 
-    case ARP_REPLY:
+    case ArpTypes::REPLY:
+        ProccessArpReply(pkt_data);
         break;
 
     default:
--- netSimulator/netSimulator/NSimulator.h
+++ netSimulator/netSimulator/NSimulator.h
@@ -1,31 +1,52 @@
 #pragma once
 
-#include "pcap.h"
 #include <stdexcept>
 #include <iostream>
+#include <map>
+
+#include "pcap.h"
+
+#undef inline
 
 const int PACKET_TYPE_POS = 12;
-const uint16_t ARP_PACKET_TYPE = 0x0806;
 
-const int ARP_OPERATION_POS = 20;
-const uint16_t ARP_REQUEST = 0x0001;
-const uint16_t ARP_REPLY = 0x0002;
+enum class ArpTypes
+{
+	ARP = 0x0608,
+	REQUEST = 0x0100,
+	REPLY = 0x0200
+};
 
-//struct ip
-//{
-//	uint8_t bytes[4];
-//};
-//
-//struct mac
-//{
-//	uint8_t bytes[6];
-//};
+enum class ArpPositions
+{
+	OPERATION = 20,
+	SENDER_MAC = 22,
+	SENDER_IP = 28,
+	TARGET_MAC = 32,
+	TARGET_IP = 38
+};
+
+struct MAC
+{
+	uint8_t bytes[6];
+};
+
+struct NetCFG
+{
+	uint32_t ip;
+	MAC mac;
+};
 
 class NSimulator
 {
 public:
 	NSimulator() : adhandle{ nullptr }, numberOfDevices{ 0 }
-	{}
+	{
+		uint8_t bytes[6] = { 0xbb, 0xbb, 0xbb, 0xbb, 0xbb, 0xbb };
+
+		netFor[0x0201a8c0].ip = 0x0101a8c0;
+		netFor[0x0201a8c0].mac = *(MAC*)(bytes);
+	}
 
 	~NSimulator() = default;
 
@@ -36,11 +57,17 @@ private:
 	pcap_t* adhandle;
 	int numberOfDevices;
 
-	//std::map<ip, mac> arpTable;
+	std::map<uint32_t, NetCFG> netFor;
+	std::map<uint32_t, MAC> arpTable;
 
-	bool IsARP(const u_char* pkt_data, int pkt_size);
+	inline bool IsARP(const u_char* pkt_data);
+	inline bool IsICMP(const u_char* pkt_data, int pktSize);
 
-	void ProccesARP(const u_char* pkt_data, int pkt_size);
+	void ProccessICMP(const u_char* pkt_data, int pktSize);
+
+	void ProccessArpRequest(const u_char* pkt_data);
+	inline void ProccessArpReply(const u_char* pkt_data);
+	void ProccesARP(const u_char* pkt_data);
 
 	void FindAllDevices(pcap_if_t** alldevs);
 	void PrintDevicesList(pcap_if_t* alldevs);
